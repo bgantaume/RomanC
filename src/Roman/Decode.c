e39f599d678cc9d0f16fc9950ac81ee90c4004d0
nbouteme <nabil@pomf.fr>
2015-02-27 17:23:01 +0100

    Fait marche le decodeur

--- src/Roman/Decode.c
+++ src/Roman/Decode.c
@@ -1,5 +1,27 @@
+#include "Stack.h"
 #include "Decode.h"
 
-int roman_decode(char* in) {
-  return 42;
+int letter_decode(char letter)
+{
+	char letters[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+	int val[]      = {  1,   5,  10,  50, 100, 500, 1000};
+
+	for(int i = 0; i < 7; ++i)
+		if(letters[i] == letter)
+			return val[i];
+	return 0;
+}
+
+int roman_decode(char *in)
+{
+	int acc = 0;
+
+	if(!*in) return 0;
+	
+	if(letter_decode(in[0]) < letter_decode(in[1]))
+		(acc = letter_decode(in[1]) - letter_decode(*in)), ++in;
+	else
+		acc = letter_decode(*in);
+
+	return acc + roman_decode(++in);
 }
--- tests/Roman/DecodeTest.cpp
+++ tests/Roman/DecodeTest.cpp
@@ -2,22 +2,47 @@
 
 extern "C"
 {
-  #include "Decode.h"
+#include "Decode.h"
 }
 
 TEST_GROUP(Roman)
 {
-  void setup()
-  {
-  }
+	void setup()
+	{
+	}
 
-  void teardown() 
-  {
-  }
+	void teardown() 
+	{
+	}
 };
 
 TEST(Roman, Decode_I)
 {
-  CHECK_EQUAL(1, roman_decode("I"));
+	CHECK_EQUAL(1, roman_decode("I"));
+}
+
+TEST(Roman, Decode_V)
+{
+	CHECK_EQUAL(5, roman_decode("V"));
+}
+
+TEST(Roman, Decode_X)
+{
+	CHECK_EQUAL(10, roman_decode("X"));
+}
+
+TEST(Roman, Decode_XV)
+{
+	CHECK_EQUAL(15, roman_decode("XV"));
+}
+
+TEST(Roman, Decode_XIV)
+{
+	CHECK_EQUAL(14, roman_decode("XIV"));
+}
+
+TEST(Roman, Decode_MCMLIV)
+{
+	CHECK_EQUAL(1954, roman_decode("MCMLIV"));
 }
 
